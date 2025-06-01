9f0ff488ddb58ca53cc59c5ff5ef0796294500f6
Pascal Kleindienst <mail@pascalkleindienst.de>
2025-06-01 14:02:41 +0200

    add polly config

--- config/text-to-speech.php
+++ config/text-to-speech.php
@@ -19,6 +19,18 @@ return [
         'voice' => env('TEXT_TO_SPEECH_GOOGLE_VOICE', 'de-DE-Chirp3-HD-Achernar'),
     ],
 
+    'polly' => [
+        'key' => env('TEXT_TO_SPEECH_POLLY_KEY'),
+        'secret' => env('TEXT_TO_SPEECH_POLLY_SECRET'),
+        'token' => env('TEXT_TO_SPEECH_POLLY_TOKEN', false),
+        'version' => env('TEXT_TO_SPEECH_POLLY_VERSION', 'latest'),
+        'region' => env('TEXT_TO_SPEECH_POLLY_REGION', 'us-east-1'),
+        'format' => env('TEXT_TO_SPEECH_POLLY_FORMAT', 'mp3'),
+        'engine' => env('TEXT_TO_SPEECH_POLLY_ENGINE', 'standard'),
+        'voice' => env('TEXT_TO_SPEECH_POLLY_VOICE', 'Joanna'),
+        'text_type' => env('TEXT_TO_SPEECH_POLLY_TEXT_TYPE', 'text'),
+    ],
+
     'system' => [
         'rate' => env('TEXT_TO_SPEECH_SYSTEM_RATE', 175),
         'pitch' => env('TEXT_TO_SPEECH_SYSTEM_PITCH', 50),
